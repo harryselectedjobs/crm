993904ead46bd6dd892ec22b8c2f7ad5396cd5f7
harryselectedjobs <harry@selected.jobs>
2026-05-01 17:10:21 +0530

    Fix DB connection - use env variables

--- repository/db_connection.py
+++ repository/db_connection.py
@@ -1,14 +1,18 @@
 import mysql.connector
 from mysql.connector import Error
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host="selectedgroupcrm.cbmo6qu6oqc6.eu-north-1.rds.amazonaws.com",
-            port=3306,
-            user="root",
-            password="root2000",
-            database="selectedgroupcrm"
+            host=os.getenv("DB_HOST"),
+            port=int(os.getenv("DB_PORT", 3306)),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
         )
 
         if connection.is_connected():
