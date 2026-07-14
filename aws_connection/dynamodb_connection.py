6539fcf8084935a31884b7aff602079b500f34a8
rahulkrguptave <rkgupta@virtualemployee.com>
2026-07-14 17:18:20 +0530

    fixing dynamodb issue

--- aws_connection/dynamodb_connection.py
+++ aws_connection/dynamodb_connection.py
@@ -7,7 +7,7 @@ load_dotenv()
 def _get_dynamodb_client():
     return boto3.resource(
         "dynamodb",
-        region_name=os.getenv("AWS_REGION", "ap-south-1"),
+        region_name="ap-south-1",
         aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
         aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
     )
